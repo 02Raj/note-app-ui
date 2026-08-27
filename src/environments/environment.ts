6b42601f5892e247f963d0292e55d72cabdc8739
SLANTCO\divya <divyanshraj02@gmail.com>
2026-08-27 16:52:54 +0530

    feat: initialize environment configuration files and add project dashboard asset

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:5000/api', 
-  apiUrl: 'https://note-app-service-x4ul.onrender.com/api', 
+  //apiUrl: 'https://note-app-service-x4ul.onrender.com/api', 
+  apiUrl: 'https://note-app-service-gao2.onrender.com/api', 
 };
